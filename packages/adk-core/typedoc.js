7a65129c4275ab10e72b419a9b73532aea324dc8
awsnitin <nggujral@amazon.com>
2023-01-19 15:18:04 -0800

    fix: move to monorepo typedoc documentation (#101)

--- packages/adk-core/typedoc.js
+++
@@ -1,5 +0,0 @@
-const baseConfig = require('../../config/typedoc');
-module.exports = {
-    ...baseConfig,
-    entryPoints: ["lib/toolkit/sdk/core/core.ts"],
-};
---
+++ typedoc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    entryPointStrategy: "packages",
+    entryPoints: [
+        "packages/adk-core",
+        "packages/adk-utils",
+        "packages/adk-model-parser",
+        "packages/codecatalyst-entities/run-summaries",
+        "packages/codecatalyst-entities/project",
+        "packages/codecatalyst-entities/space"
+   ]
+}
