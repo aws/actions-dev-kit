23e813d7595db62cdda1411a92a270a27ab15bb4
kshp <pkoshla@gmail.com>
2023-02-19 09:56:49 -0800

    fix: synchronize codecatalyst action schema JSON with AMS

--- packages/adk-model-parser/lib/sanitizer/sanitizer.ts
+++ packages/adk-model-parser/lib/sanitizer/sanitizer.ts
@@ -13,14 +13,8 @@ import { isString } from '@codecatalyst/adk-utils/lib/util/util';
 export function sanitizeModel(model: Model): Model {
     model['SchemaVersion'] = escape(model.SchemaVersion);
 
-    if (model.Author) {
-        model['Author'] = escape(model.Author);
-    }
     if (model.Description) {
         model['Description'] = escape(model.Description);
-    }
-    if (model.Id) {
-        model['Id'] = escape(model.Id);
     }
     if (model.Name) {
         model['Name'] = escape(model.Name);
@@ -30,9 +24,7 @@ export function sanitizeModel(model: Model): Model {
     }
 
     if (model.Configuration !== undefined) {
-
         const sanitizedInputsMap: { [key: string]: Input } = {};
-
         Object.entries(model.Configuration!).map(([key, value]) => {
             const sanitizedKey = escape(key);
             const sanitizedInput = escapeConfigurationInput(value);
@@ -41,30 +33,23 @@ export function sanitizeModel(model: Model): Model {
         model['Configuration'] = sanitizedInputsMap;
     }
 
-    if (model.Runs) {
-        if (model.Runs.Main) {
-            model.Runs['Main'] = escape(model.Runs.Main);
-        }
-        if (model.Runs.Using) {
-            model.Runs['Using'] = escape(model.Runs.Using);
-        }
-    }
+    model.Runs!['Main'] = escape(model.Runs!.Main);
+    model.Runs!['Using'] = escape(model.Runs!.Using);
 
     return model;
 }
 
 function escapeConfigurationInput(input: Input): Input {
-    if (input.Description) {
-        input['Description'] = escape(input.Description);
-    }
-    if (input.Type && isString(input.Type)) {
-        input['Type'] = escape(input.Type);
-    }
-    if (input.DisplayName) {
+    input['Description'] = escape(input.Description);
+
+    if (input['DisplayName']) {
         input['DisplayName'] = escape(input.DisplayName);
     }
-    if (input.Default && isString(input.Default)) {
-        input['Default'] = escape(input.Default);
+    if (input['Default'] && isString(input.Default)) {
+        input['Default'] = escape(input.Default as string);
+    }
+    if (input['Type'] && isString(input.Type)) {
+        input['Type'] = escape(input.Type as string);
     }
 
     return input;
