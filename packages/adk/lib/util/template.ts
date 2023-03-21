6c3e918a79ddabd70ee41c0338e3c96f1c65cc87
kshp <koshlap@amazon.com>
2023-03-21 08:24:09 -0700

    add .semgrepignore to ignore generated files and findings that cannot be exploited

--- packages/adk/lib/util/template.ts
+++ packages/adk/lib/util/template.ts
@@ -1,8 +1,7 @@
 export function applyTemplate(templateContents: string, templateParam: { [key: string]: string }): string {
     for (let key in templateParam) {
         let value = templateParam[key];
-        let searchKey = `%%${key}%%`;
-        let keyReplace = new RegExp(searchKey, 'gi');
+        let keyReplace = new RegExp( `%%${key}%%`, 'gi');
         templateContents = templateContents.replace(keyReplace, value);
     }
     return templateContents;
