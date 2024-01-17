d270a3c17b9b807c709d64a849dd19420ffc168f
alexforsyth <alex-git@amazon.com>
2024-01-16 22:52:19 -0500

    feat: move packages into nice places

---
+++ packages/entities/space/.projenrc.ts
@@ -0,0 +1,14 @@
+import * as fs from 'fs';
+import { TypescriptConstruct } from '@aws/adk-typescript-construct';
+
+const existingPackage = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
+const project = new TypescriptConstruct({
+  defaultReleaseBranch: 'main',
+  packageName: existingPackage.name,
+  description: existingPackage.description,
+  name: existingPackage.name,
+  version: existingPackage.version || '0.0.0',
+  deps: [],
+  devDeps: ['projen', 'ts-node@^10', '@aws/adk-typescript-construct'],
+});
+project.synth();
